0d94cab9ca3aa6875ce312e5c54f602626e01b38
jakecoltman <jakecoltman@sky.com>
2016-05-06 10:28:02 +0100

    Update to first non - dev version

--- setup.py
+++ setup.py
@@ -3,14 +3,10 @@ from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-#with open(path.join(here, 'README.md')) as f:
-#    long_description = f.read()
-
 setup(
     name='pyBMA',
     packages=["pyBMA"],
-    version='0.1.dev15',
+    version='0.1.1',
     description='Bayesian Model Averaging in Python',
     long_description="Bayesian Model Averaging in Python",
     url='https://github.com/JakeColtman/pyBMA',
